9f44db05c1f7b0bf7e1ff0f345bd57d067d96bee
Adrian Humphreys <adrhumphreys@gmail.com>
2020-08-13 09:29:10 +1200

    Update LoadFixtures.php

--- src/tasks/LoadFixtures.php
+++ src/tasks/LoadFixtures.php
@@ -8,6 +8,7 @@ use AdrHumphreys\Fixtures\Executor;
 use AdrHumphreys\Fixtures\Loader;
 use AdrHumphreys\Fixtures\Logger;
 use AdrHumphreys\Fixtures\Purger;
+use SilverStripe\Control\Director;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Dev\BuildTask;
 
@@ -24,6 +25,17 @@ class LoadFixtures extends BuildTask
      */
     public function run($request): void
     {
+        if (Director::isLive()) {
+            if ($request->getVar('force') !== 'true') {
+                Logger::log('This is not designed to be run on production');
+
+                return;
+            }
+
+            Logger::orange('This is not designed to be run on production');
+            Logger::log('But you have forced it to be run in production');
+        }
+
         $directory = $request->getVar('directory');
 
         $append = $request->getVar('append') === 'true';
